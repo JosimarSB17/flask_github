8795527d2ebdeec02443c195668e8392dd8ded07
Josimar <email@>
2025-10-17 19:32:36 -0500

    Final code

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template, request  # NOT the same as requests 
-
+from github_api import get_github_user
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') # this takes you to the homepage
 def homepage():
     return render_template('index.html')
 
+@app.route('/get_user') # don't forget the slash
+def get_user_info():
+    # Get the username from github api an display some info 
+    print('Form data is ', request.args)  # requests.args is a dictionary
+    username = request.args.get('username')  # this is safer it returns None if not found
+    user_info, error_message = get_github_user(username) # this is a dictionary
+    if error_message:
+        return render_template('error.html', error=error_message)
+    else:
+        return render_template('github.html', user_info=user_info)
 
 if __name__ == '__main__':
     app.run()
--- github_api.py
+++ github_api.py
@@ -1,1 +1,28 @@
 # TODO make requests to github API
+
+import requests
+
+def get_github_user(username):
+    # dealing with multiple different errors here
+    # returning a tuple if this succed or fail
+    try:
+        response = requests.get(f'https://api.github.com/users/{username}')
+        if response.status_code == 404: # not found
+            return None, f'User {username} not found'
+        response.raise_for_status()  # raise error for other bad status codes
+        response_json = response.json()
+        user_info = extract_user_info(response_json)
+        return user_info, None  # no error
+    except Exception as e:
+        return None, 'Error connecting to GitHub '
+
+
+def extract_user_info(json_response):
+    return {
+        'login': json_response.get('login'),
+        'name': json_response.get('name'),
+        'avatar_url': json_response.get('avatar_url'),
+        'home_page': json_response.get('html_url'),
+        'repos': json_response.get('public_repos'),
+        
+    }
